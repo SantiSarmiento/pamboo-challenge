87ce948ea28c6e66369d2f0c57b033c95d96ef00
@santisarmiento54 <santisarmiento54@gmail.com>
2023-05-07 12:07:15 -0300

    iconos instalados

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,21 @@
+import React from 'react';
 import { HStack, HamburgerIcon, Text, VStack } from "native-base";
-import React from "react";
 import { titulos } from "../../helpers/Colors"
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const Header = () => {
 
+    const navigation = useNavigation()
+
     function obtenerFechaFormateada() {
         const fecha = new Date();
-
         const dia = fecha.getDate();
         const mes = fecha.toLocaleString('es-ES', { month: 'long' });
         const anio = fecha.getFullYear();
 
         return `${mes} ${dia}, ${anio}`
-
     }
-
 
     return (
         <HStack alignItems={"center"} justifyContent={"space-between"} w={"90%"} alignSelf={"center"} mt={8} mb={2}>
@@ -22,7 +23,9 @@ const Header = () => {
                 <Text fontSize={"xl"} fontWeight={"medium"}>{obtenerFechaFormateada()}</Text>
                 <Text fontSize={"xl"} fontWeight={"medium"} color={titulos} >Hoy</Text>
             </VStack>
-            <HamburgerIcon size={"xl"} color={titulos} />
+            <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                <HamburgerIcon color={titulos} size={"xl"} />
+            </TouchableOpacity>
         </HStack>
     )
 }
